f55aa381e8d81096256a96591a61c5205d1b2185
albinodasilva00-ui <albinodasilva00@gmail.com>
2026-07-02 17:51:43 +0700

    Update 00_Config.js

--- gee/gee/moduleA_classification/00_Config.js
+++ gee/gee/moduleA_classification/00_Config.js
@@ -1,1 +1,17 @@
+/*
+====================================================
+TL-NFMS
 
+Module A
+
+Configuration File
+
+Author:
+Albino Da Silva Barbosa
+
+Universitas Gadjah Mada
+
+====================================================
+*/
+
+print("TL-NFMS Configuration Loaded");
